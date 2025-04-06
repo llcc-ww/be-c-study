3477b678953c9661db35d6ce762eb51c3a38cf39
chaewon <agneskellywon@gmail.com>
2025-04-07 07:06:20 +0900

    second init commit

---
+++ src/main/java/com/study/bestudy/BestudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.bestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/bestudy/BestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.bestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
